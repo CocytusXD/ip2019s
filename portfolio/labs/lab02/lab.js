774472f842f12528f4df60321945fc2077ca585f
CocytusXD <52515305+CocytusXD@users.noreply.github.com>
2019-07-05 17:56:36 +0800

    Add files via upload

--- portfolio/labs/lab02/lab.js
+++ portfolio/labs/lab02/lab.js
@@ -2,8 +2,28 @@
 
 */
 
+function update(){
+    var x = document.getElementById('in1').value;
+    var ele = document.getElementById('info1');
+    var y = '';
+
+    for (var i = 0; i < x.length; i++){
+        y += x.charAt(i)
+        y += ' U+';
+        y += x.charCodeAt(i).toString(16).toUpperCase();
+        y += '<br>' ;
+}
+
+ele.innerHTML = y;
+
+document.getElementById('info3').innerHTML =
+document.getElementById('in1').value.replace('黑金卡', '垃圾卡');
+}
 
 function start(e) {
+    document.getElementById('in1').addEventListener("input", update);
+    update();
+
 
 }
 
